49a3be26f81576e84262e4e3bbc70e49be4faf04
Nikolai Baida <nikolai.baida@gmail.com>
2022-11-03 17:47:23 +0100

    hello world

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+
+// header
+  // nav bar
+    // left logo
+    // 2 links to views
+// main
+  // picker 
+    // view 1
+
+    // view 2 
+// footer
+
+
+
+</script>
+
+<template>
+  
+  <h1 class="text-center bg-red-100">lol</h1>
+
+
+</template>
+
+<style scoped>
+
+</style>
